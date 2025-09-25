6d5f36289a8088975ff8ba2320ebb452a23bf804
Mohamedawad114 <mohamedahmedawad180@gmail.com>
2025-09-25 20:30:35 +0300

    fix something

--- src/utiles/encryption.js
+++ src/utiles/encryption.js
@@ -4,9 +4,9 @@ env.config()
 const key =process.env.CRYPTO_KEY
 const IV=crypto.randomBytes(16)
 function encryption(text){
-    const buffer=Buffer.from(text)
-    const cipher=crypto.createCipheriv("aes-256-cbc",key,IV)
-    let encrypted=cipher.update(buffer,'utf-8','hex')
+    const buffer=Buffer.from(key)
+    const cipher=crypto.createCipheriv("aes-256-cbc",buffer,IV)
+    let encrypted=cipher.update(text,'utf-8','hex')
     encrypted+=cipher.final('hex')
     return IV.toString('hex')+":"+encrypted
 }
